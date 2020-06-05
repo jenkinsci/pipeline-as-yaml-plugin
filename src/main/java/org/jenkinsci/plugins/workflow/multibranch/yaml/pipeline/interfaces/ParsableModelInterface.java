970b2bf12893f16dd2f0bc5bdd7512a7448b68c6
Aytunc BEKEN <aytunc@authenteq.com>
2020-06-05 15:53:14 +0200

    interfaces package javadoc

--- src/main/java/org/jenkinsci/plugins/workflow/multibranch/yaml/pipeline/interfaces/ParsableModelInterface.java
+++ src/main/java/org/jenkinsci/plugins/workflow/multibranch/yaml/pipeline/interfaces/ParsableModelInterface.java
@@ -1,7 +1,14 @@
 package org.jenkinsci.plugins.workflow.multibranch.yaml.pipeline.interfaces;
 
+/**
+ * Interface for Model Classes
+ */
 public interface ParsableModelInterface {
 
+    /**
+     * Convert model to Groovy Syntax
+     * @return {@link String}
+     */
     String toGroovy();
 
 }
--- src/main/java/org/jenkinsci/plugins/workflow/multibranch/yaml/pipeline/interfaces/ParserInterface.java
+++ src/main/java/org/jenkinsci/plugins/workflow/multibranch/yaml/pipeline/interfaces/ParserInterface.java
@@ -2,7 +2,15 @@ package org.jenkinsci.plugins.workflow.multibranch.yaml.pipeline.interfaces;
 
 import java.util.Optional;
 
+/**
+ * Interface for Parser classes
+ * @param <T> Generic Type
+ */
 public interface ParserInterface<T > {
 
+    /**
+     * Parse Yaml to Model
+     * @return {@link Optional<T>}
+     */
     Optional<T> parse();
 }
