73f58f79a424a26e304c2be92d9ba9d2dd34626d
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-03-05 13:47:13 +0100

    Migrate GitSampleRepoRule to JUnit5

--- src/test/java/io/jenkins/plugins/pipeline/PipelineAsYamlScmFlowDefinitionTest.java
+++ src/test/java/io/jenkins/plugins/pipeline/PipelineAsYamlScmFlowDefinitionTest.java
@@ -1,5 +1,8 @@
 package io.jenkins.plugins.pipeline;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import hudson.model.Result;
 import hudson.plugins.git.GitSCM;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
@@ -7,6 +10,7 @@ import java.util.List;
 import java.util.UUID;
 import jenkins.plugins.git.GitSCMSource;
 import jenkins.plugins.git.GitSampleRepoRule;
+import jenkins.plugins.git.junit.jupiter.WithGitSampleRepo;
 import jenkins.plugins.git.traits.BranchDiscoveryTrait;
 import jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait;
 import org.apache.commons.io.FileUtils;
@@ -15,54 +19,54 @@ import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.jenkinsci.plugins.workflow.libs.GlobalLibraries;
 import org.jenkinsci.plugins.workflow.libs.LibraryConfiguration;
 import org.jenkinsci.plugins.workflow.libs.SCMSourceRetriever;
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
-public class PipelineAsYamlScmFlowDefinitionTest {
+@WithJenkins
+@WithGitSampleRepo
+class PipelineAsYamlScmFlowDefinitionTest {
 
-    @Rule
-    public JenkinsRule jenkinsRule = new JenkinsRule();
+    private JenkinsRule jenkinsRule;
+    private GitSampleRepoRule gitRepo;
+    private final String yamlJenkinsFileName = "Jenkinsfile.yaml";
 
-    @Rule
-    public GitSampleRepoRule sourceCodeRepo = new GitSampleRepoRule();
-
-    @Rule
-    public GitSampleRepoRule libraryCodeRepo = new GitSampleRepoRule();
-
-    private String yamlJenkinsFileName = "Jenkinsfile.yaml";
-    private String[] scmBranches = {"feature", "hotfix"};
+    @BeforeEach
+    void setUp(JenkinsRule jenkinsRule, GitSampleRepoRule gitRepo) {
+        this.jenkinsRule = jenkinsRule;
+        this.gitRepo = gitRepo;
+    }
 
     @Test
-    public void testAllInOne() throws Exception {
+    void testAllInOne() throws Exception {
         String yamlJenkinsFileContent = FileUtils.readFileToString(
                 new File("src/test/resources/job/pipelineAllInOne.yml"), StandardCharsets.UTF_8);
-        this.sourceCodeRepo.init();
-        this.sourceCodeRepo.write(this.yamlJenkinsFileName, yamlJenkinsFileContent);
-        this.sourceCodeRepo.git("add", this.yamlJenkinsFileName);
-        this.sourceCodeRepo.git("commit", "--all", "--message=InitRepoWithFile");
+        this.gitRepo.init();
+        this.gitRepo.write(this.yamlJenkinsFileName, yamlJenkinsFileContent);
+        this.gitRepo.git("add", this.yamlJenkinsFileName);
+        this.gitRepo.git("commit", "--all", "--message=InitRepoWithFile");
         WorkflowJob workflowJob = this.jenkinsRule.createProject(
                 WorkflowJob.class, UUID.randomUUID().toString());
         workflowJob.setDefinition(new PipelineAsYamlScmFlowDefinition(
-                this.yamlJenkinsFileName, new GitSCM(this.sourceCodeRepo.toString()), false));
+                this.yamlJenkinsFileName, new GitSCM(this.gitRepo.toString()), false));
         workflowJob.scheduleBuild2(0);
         jenkinsRule.waitUntilNoActivity();
         WorkflowRun workflowRun = workflowJob.getLastBuild();
         System.out.println(workflowRun.getLog());
-        Assert.assertEquals("SUCCESS", workflowRun.getResult().toString());
+        assertEquals(Result.SUCCESS, workflowRun.getResult());
     }
 
     @Test
-    public void testWithLibrary() throws Exception {
+    void testWithLibrary() throws Exception {
 
-        this.libraryCodeRepo.init();
-        this.libraryCodeRepo.mkdirs("vars");
-        this.libraryCodeRepo.write("vars/customSteps.groovy", "def customStep(message)  {  echo \"${message}\" }");
-        this.libraryCodeRepo.git("add", "vars/customSteps.groovy");
-        this.libraryCodeRepo.git("commit", "--all", "--message=InitRepoWithStep");
+        this.gitRepo.init();
+        this.gitRepo.mkdirs("vars");
+        this.gitRepo.write("vars/customSteps.groovy", "def customStep(message)  {  echo \"${message}\" }");
+        this.gitRepo.git("add", "vars/customSteps.groovy");
+        this.gitRepo.git("commit", "--all", "--message=InitRepoWithStep");
 
-        GitSCMSource source = new GitSCMSource(this.libraryCodeRepo.toString());
+        GitSCMSource source = new GitSCMSource(this.gitRepo.toString());
         source.setTraits(List.of(new BranchDiscoveryTrait(), new WildcardSCMHeadFilterTrait("*", "")));
         LibraryConfiguration sharedLibraryConfiguration =
                 new LibraryConfiguration("customSharedLibrary", new SCMSourceRetriever(source));
@@ -72,19 +76,18 @@ public class PipelineAsYamlScmFlowDefinitionTest {
 
         String yamlJenkinsFileContent = FileUtils.readFileToString(
                 new File("src/test/resources/job/pipelineTestWithLibrary.yml"), StandardCharsets.UTF_8);
-        this.sourceCodeRepo.init();
-        this.sourceCodeRepo.write(this.yamlJenkinsFileName, yamlJenkinsFileContent);
-        this.sourceCodeRepo.git("add", this.yamlJenkinsFileName);
-        this.sourceCodeRepo.git("commit", "--all", "--message=InitRepoWithFile");
+        this.gitRepo.write(this.yamlJenkinsFileName, yamlJenkinsFileContent);
+        this.gitRepo.git("add", this.yamlJenkinsFileName);
+        this.gitRepo.git("commit", "--all", "--message=InitRepoWithFile");
         WorkflowJob workflowJob = this.jenkinsRule.createProject(
                 WorkflowJob.class, UUID.randomUUID().toString());
         workflowJob.setDefinition(new PipelineAsYamlScmFlowDefinition(
-                this.yamlJenkinsFileName, new GitSCM(this.sourceCodeRepo.toString()), false));
+                this.yamlJenkinsFileName, new GitSCM(this.gitRepo.toString()), false));
         workflowJob.scheduleBuild2(0);
         jenkinsRule.waitUntilNoActivity();
         WorkflowRun workflowRun = workflowJob.getLastBuild();
         System.out.println(workflowRun.getLog());
         this.jenkinsRule.assertLogContains("customEcho", workflowRun);
-        Assert.assertEquals("SUCCESS", workflowRun.getResult().toString());
+        assertEquals(Result.SUCCESS, workflowRun.getResult());
     }
 }
